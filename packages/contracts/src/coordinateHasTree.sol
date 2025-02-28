f6434568f3273315a2b659a55b239be7f31984a9
yonadaaa <fraserdscott@gmail.com>
2025-02-27 22:22:22 -0700

    feat: increase tree density

--- packages/contracts/src/coordinateHasTree.sol
+++ packages/contracts/src/coordinateHasTree.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.24;
 
 uint256 constant TREE_PROBABILITY_DIVISOR = 100;
-uint256 constant TREE_PROBABILITY_COMPARATOR = 3;
+uint256 constant TREE_PROBABILITY_COMPARATOR = 4;
 
 function coordinateHasTree(int32 x, int32 y) pure returns (bool) {
   bytes32 hash = keccak256(abi.encode(x, y));
