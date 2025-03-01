d70085c0aa0e4c6e8ba6ab5e086efcef7eb920c8
yonadaaa <fraserdscott@gmail.com>
2025-03-01 00:47:17 -0700

    fix: file names

--- packages/client/src/game/agent/systems.ts
+++ packages/client/src/game/agent/systems.ts
@@ -1,5 +1,5 @@
-import { MoveSystem } from "./moveSystem";
-import { HarvestSystem } from "./harvestSystem";
-import { StealSystem } from "./stealSystem";
+import { MoveSystem } from "./MoveSystem";
+import { HarvestSystem } from "./HarvestSystem";
+import { StealSystem } from "./StealSystem";
 
 export const systems = [MoveSystem, HarvestSystem, StealSystem];
