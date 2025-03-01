a3cadbe4c095b92ec615cef39c8347558dd1593b
yonadaaa <fraserdscott@gmail.com>
2025-03-01 00:13:09 -0700

    refactor: use pure functions

--- packages/contracts/src/StealSystem.sol
+++ packages/contracts/src/StealSystem.sol
@@ -4,13 +4,13 @@ pragma solidity >=0.8.24;
 import { System } from "@latticexyz/world/src/System.sol";
 import { Player, PlayerData } from "./codegen/tables/Player.sol";
 
-contract StealSystem is System {
-  function getManhattanDistance(PlayerData memory a, PlayerData memory b) internal pure returns (int32) {
-    int32 xDiff = a.x > b.x ? a.x - b.x : b.x - a.x;
-    int32 yDiff = a.y > b.y ? a.y - b.y : b.y - a.y;
-    return xDiff + yDiff;
-  }
+function getManhattanDistance(PlayerData memory a, PlayerData memory b) pure returns (int32) {
+  int32 xDiff = a.x > b.x ? a.x - b.x : b.x - a.x;
+  int32 yDiff = a.y > b.y ? a.y - b.y : b.y - a.y;
+  return xDiff + yDiff;
+}
 
+contract StealSystem is System {
   function steal(address targetAccount) public {
     address account = _msgSender();
     PlayerData memory player = Player.get(account);
